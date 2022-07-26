c268528c996585c2c746222a03105f6fd6ad498e
jococo1221 <jococo1221@gmail.com>
2022-07-26 13:36:42 -0400

    pinky keys added. tried to add mouse

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -6,6 +6,15 @@
 #pragma once
 
 
+// CUSTOM Mouse keys parmeters (I DON'T THINK THIS IS WORKING)
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+//#define MOUSEKEY_TIME_TO_MAX    64
+#undef MOUSEKEY_WHEEL_MAX_SPEED 
+#define MOUSEKEY_WHEEL_MAX_SPEED   	20
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX 
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 	20
+
 //invert U_MOUSE and U_NAV
 /*
 #define MIRYOKU_LAYER_BASE \
@@ -14,3 +23,16 @@ LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G
 LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH),\
 U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_MOUSE,KC_SPC),  LT(U_NAV,KC_TAB),LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,              U_NP
 */
+
+#define MIRYOKU_MAPPING(\
+       K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09,\
+       K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19,\
+       K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,\
+       N30,   N31,   K32,   K33,   K34,          K35,   K36,   K37,   N38,   N39\
+)\
+LAYOUT_split_3x6_3(\
+KC_NO, K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09,   CG_TOGG,\
+KC_BTN1, K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19,   KC_NO,\
+KC_BTN2, K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,   KC_NO,\
+                     K32,   K33,   K34,          K35,   K36,   K37\
+)
